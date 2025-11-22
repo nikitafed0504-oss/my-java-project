52a2a0be26282b6de994ecf82b7a0a4a1610df9d
Nikita Fedorov <nikitafed0504@gmail.com>
2025-11-22 15:39:26 +0300

    Initial commit: Java project with CI/CD

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+﻿package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testAppHasGreeting() {
+        App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+}
